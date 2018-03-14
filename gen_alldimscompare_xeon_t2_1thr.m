0b7b57a79b1f03932517f423bd855b2e0f028ad1
ahbarnett <ahb@math.dartmouth.edu>
2018-03-14 09:45:52 -0400

    added 2d & 3d data except t2 1thr

---
+++ gen_alldimscompare_xeon_t2_1thr.m
@@ -0,0 +1,23 @@
+% run all timing comparison expts, writing to .mat files
+% barnett 3/14/18
+
+cpu='xeon';
+o = []; o.memcpu=1;
+
+for multithreaded=0%[1 0]
+
+  if multithreaded, nthreads = 24; M=1e8; NN=1e7;  % multi -------- bigger expt
+  else, nthreads = 1; M=1e7; NN=1e6;  % single -------- smaller expt
+  end
+
+  for ty = 2%[1 2]
+    for nudist = [0 4]
+      for dim = [2 3]
+        N=round(NN^(1/dim)); if mod(N,2)==1, N=N+1; end   % modes per dim, even
+        o.nfftpres = 2; if dim==3 & M>1e7, o.nfftpres=1; end
+        outname=sprintf('results/%s/%dd%d_nudist%d_N%d_M%d_%dthr_nfftpres%d',cpu,dim,ty,nudist,N,M,nthreads,o.nfftpres);
+        benchallcodes(ty,dim,N,M,nudist,multithreaded,outname,o);
+      end
+    end
+  end
+end
